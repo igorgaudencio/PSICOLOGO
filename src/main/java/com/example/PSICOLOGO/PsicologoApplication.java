90e238211e1564e73772d3928a701c14507d2df5
igorgaudencio <igorgaudencio@live.com>
2024-11-19 22:00:27 -0300

    Base do software

---
+++ src/main/java/com/example/PSICOLOGO/PsicologoApplication.java
@@ -0,0 +1,13 @@
+package com.example.PSICOLOGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicologoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicologoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PSICOLOGO/PsicologoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PSICOLOGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicologoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
