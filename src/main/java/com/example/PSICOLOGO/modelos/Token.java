a25e7c0a1e14beb9e5b9bede3cac1940d88e6887
igorgaudencio <igorgaudencio@live.com>
2024-11-21 18:41:41 -0300

    Token e repositorio

---
+++ src/main/java/com/example/PSICOLOGO/modelos/Token.java
@@ -0,0 +1,31 @@
+package com.example.PSICOLOGO.modelos;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validateAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+
+}
